c9bd7d205172bb1891610c5c8790fd745e9b33de
Rodney Maxwell <rodmxw11@gmail.com>
2020-07-23 00:48:21 -0400

    FrinkVisitor finds indented code blocksgit add frink-notebook.groovy git add frink-notebook.groovy

--- frink-notebook.groovy
+++ frink-notebook.groovy
@@ -8,21 +8,38 @@ import frink.parser.Frink
 import org.commonmark.node.*
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class frink_notebook {
+
+   public static class FrinkVisitor extends AbstractVisitor {
+      public void visit(IndentedCodeBlock indentedCodeBlock) {
+         println "=======> FRINK ======>\n${indentedCodeBlock.literal}\n=============================\n"
+      }
+   }
+
+   private static String fname = 'finance-calca.md'
+
+   public static void main(String[] args) {
+
+      Parser parser = Parser.builder().build()
+      Node document = parser.parseReader(new File(fname).newReader())
+      FrinkVisitor frink_visitor = new FrinkVisitor()
+
+      document.accept(frink_visitor)
+
+      HtmlRenderer renderer = HtmlRenderer.builder().build()
+      String html_out = renderer.render(document)
+      println html_out
 
 
-String fname = 'finance-calca.md'
+      Frink frink = new Frink()
 
-Parser parser = Parser.builder().build()
-Node document = parser.parseReader(new File(fname).newReader())
-HtmlRenderer renderer = HtmlRenderer.builder().build()
-String html_out = renderer.render(document)
-println html_out
+      String frink_expr = 'gallon^(1/3) -> inches'
 
+      String results = frink.parseString(frink_expr)
 
-Frink frink = new Frink()
-
-def frink_expr = 'gallon^(1/3) -> inches'
-
-def results = frink.parseString(frink_expr)
-
-println results.replaceAll(/],/, '],\n')
+      println results.replaceAll(/],/, '],\n')
+   }
+}
